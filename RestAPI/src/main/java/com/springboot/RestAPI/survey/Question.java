117f9c1e8dd1bfeceaad7680f496737bc6a21f72
Sri Kumar <kssrikumar180703@gmail.com>
2023-02-02 18:04:58 +0530

    Added survey

--- RestAPI/src/main/java/com/springboot/RestAPI/survey/Question.java
+++ RestAPI/src/main/java/com/springboot/RestAPI/survey/Question.java
@@ -2,44 +2,17 @@ package com.springboot.RestAPI.survey;
 
 import java.util.List;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@AllArgsConstructor
+@ToString
 public class Question {
-	public Question() {
-		super();
-	}
-
-	public Question(String id, String description, List<String> options, String correctAnswer) {
-		super();
-		this.id = id;
-		this.description = description;
-		this.options = options;
-		this.correctAnswer = correctAnswer;
-	}
-
-	@Override
-	public String toString() {
-		return "Question [id=" + id + ", description=" + description + ", options=" + options + ", correctAnswer="
-				+ correctAnswer + "]";
-	}
-
 	private String id;
 	private String description;
 	private List<String> options;
 	private String correctAnswer;
-
-	public String getId() {
-		return id;
-	}
-
-	public String getDescription() {
-		return description;
-	}
-
-	public List<String> getOptions() {
-		return options;
-	}
-
-	public String getCorrectAnswer() {
-		return correctAnswer;
-	}
 
 }
--- RestAPI/src/main/java/com/springboot/RestAPI/survey/Survey.java
+++ RestAPI/src/main/java/com/springboot/RestAPI/survey/Survey.java
@@ -4,15 +4,7 @@ import java.util.List;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
 
-@Getter
-@Setter
-@ToString
-@NoArgsConstructor
 @AllArgsConstructor
 
 @Data
